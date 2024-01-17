0c23c84dd97796858d320fb9234dbdf3c6b6c39e
Daniel <dgorianz@tealsystems.com>
2024-01-17 13:25:36 -0400

    not found page added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { TopMenu } from './ui/top-menu/TopMenu';
+export { PageNotFound } from './ui/not-found/PageNotFound';
---
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -0,0 +1,25 @@
+import { titleFont } from '@/config/fonts'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export const PageNotFound = () => {
+  return (
+    <div className='flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle'>
+        <div className='text-center px-5 mx-5'>
+            <h2 className={`${titleFont.className} antialiased text-9xl`}>404</h2>
+            <p className='font-semibold text-xl'>Whoops!.. Page Not Found</p>
+            <p className='font-light'>
+                <span>You can go back to the </span>
+                <Link href={'/'} className='font-bold hover:underline transition-all text-amber-900'>
+                    Home
+                </Link>
+            </p>
+        </div>
+        <div className='px-5 mx-5'>
+            <Image src={'/imgs/not_found_page.png'} alt='Startman' className='p-5 sm:p-0' width={400} height={400}/>
+
+        </div>
+    </div>
+  )
+}
