f255e189eafb5acc98ccd58bfe6dc05d5551587b
Daniel <dgorianz@tealsystems.com>
2024-01-17 18:49:36 -0400

    sidebar added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export { TopMenu } from './ui/top-menu/TopMenu';
 export { PageNotFound } from './ui/not-found/PageNotFound';
-export { Title } from './ui/title/Title';
 export { ProductGrid } from './products/product-grid/ProductGrid';
 export { ProductGridItem } from './products/product-grid/ProductGridItem';
+export { Sidebar } from './ui/sidebar/Sidebar';
+export { Title } from './ui/title/Title';
+export { TopMenu } from './ui/top-menu/TopMenu';
